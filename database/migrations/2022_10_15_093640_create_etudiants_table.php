1f4b7c60995b6753020943e8fe58f80039069126
TonyBimenyi <83509726+TonyBimenyi@users.noreply.github.com>
2022-10-15 05:50:24 -0700

    all migrations

---
+++ database/migrations/2022_10_15_093640_create_etudiants_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('matricule');
+            $table->string('fname');
+            $table->string('lname');
+            $table->string('sexe');
+            $table->string('email');    
+            $table->string('class');
+            $table->foreignId('spec_id')->constrained('specialisations');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('etudiants');
+    }
+};
---
+++ database/migrations/2022_10_15_095542_create_teachers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $table->string('matricule');
+            $table->string('fname');
+            $table->string('lname');
+            $table->string('sexe');
+            $table->string('email');    
+            $table->foreignId('ic_course')->constrained('courses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+};
