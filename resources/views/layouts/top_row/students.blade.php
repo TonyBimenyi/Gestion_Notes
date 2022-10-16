eeb50ce22728e226456da72b8d0fac85423182b4
TonyBimenyi <83509726+TonyBimenyi@users.noreply.github.com>
2022-10-16 15:00:19 -0700

    students top menu with navside design changes

--- resources/views/layouts/top_row/students.blade.php
+++ resources/views/layouts/top_row/students.blade.php
@@ -1,10 +1,10 @@
 <div class="second_row">
     <div class="menu">
         <div class="text ">
-           <a href="#"><p id="active">LIST OF STUDENTS</p></a> 
+           <a href="{{route('students')}}"><p id="active">LIST OF STUDENTS</p></a> 
         </div>
         <div class="text">
-            <a href="#"><p>NEW STUDENT</p></a> 
+            <a href="{{route('students.form')}}"><p>NEW STUDENT</p></a> 
         </div>
     </div>
 </div>
